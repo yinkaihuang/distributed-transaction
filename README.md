921f5e59a5c878275715a61baa3510fa21eb0418
yinchong <yin.chong@intellif.com>
2019-08-27 16:42:01 +0800

    创建分布式事务框架

---
+++ README.md
@@ -0,0 +1,7 @@
+# 分布式事务使用
+
+## 模块讲解
+```
+1.RM（ResourceManager）   资源管理器
+2.TM(TransactionManager)   事务协调器
+```
---
+++ transaction-manager/src/main/java/cn/bucheng/tm/Template.java
@@ -0,0 +1,11 @@
+package cn.bucheng.tm;
+
+/**
+ * @author ：yinchong
+ * @create ：2019/8/27 16:41
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class Template {
+}
