27df0aa65d2e12dfa3ab091d0de301c048573426
yinchong <yin.chong@intellif.com>
2019-08-28 14:21:36 +0800

    添加TM集成到注册中的使用

--- transaction-manager/src/main/java/cn/bucheng/tm/TMApplication.java
+++ transaction-manager/src/main/java/cn/bucheng/tm/TMApplication.java
@@ -2,6 +2,7 @@ package cn.bucheng.tm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author ：yinchong
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @modified By：
  * @version:
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TMApplication {
     public static void main(String[] args) {
