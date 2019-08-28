d0578a6c25cedd1a22b9ffbc21023b4cfa927523
yinchong <yin.chong@intellif.com>
2019-08-28 10:20:57 +0800

    修复一服务调用A到B再到A出现的连接文件

--- resource-manager/src/main/java/cn/bucheng/rm/feign/FeignRequestInterceptor.java
+++ resource-manager/src/main/java/cn/bucheng/rm/feign/FeignRequestInterceptor.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
 
+/**
+ * 如果这里无法将请求头中事务标记传递过去，请查看这个对象调用是不是放在hystrix的线程中进行调用
+ * 导致上下文中的内容无法正常传递
+ */
 @Component
 public class FeignRequestInterceptor implements RequestInterceptor {
     @Override
